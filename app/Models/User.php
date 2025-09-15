415630b1fcca8219a1ae4ff9ea8fd7a57835e6cc
Tohadi <toadiasley243@gmail.com>
2025-09-15 15:58:25 +0700

    add transaksi relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Wsp\TransaksiModel;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -49,5 +50,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function transaksi()
+    {
+        return $this->hasMany(TransaksiModel::class, 'id_user');
     }
 }
