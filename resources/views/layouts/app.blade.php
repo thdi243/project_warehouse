d96021f078445e48f97ce623dc5f29a8150afa76
Tohadi <toadiasley243@gmail.com>
2025-09-17 11:57:03 +0700

    update template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,7 @@
         <link href="{{ asset('material/assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
 
         <!-- custom Css-->
+        <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" type="text/css" />
         <link href="{{ asset('material/assets/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
         <link href="{{ asset('material/assets/libs/aos/aos.css') }}" rel="stylesheet" type="text/css" />
 
