41002793e9b0981d2b7d8a53afb3dd2dee591419
Tohadi <toadiasley243@gmail.com>
2025-10-30 15:55:08 +0700

    add table temp note

---
+++ app/Models/Wfg/stock_opname/WfgSopTempNoteModel.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models\Wfg\stock_opname;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class WfgSopTempNoteModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'wfg_sop_temp_note';
+
+    protected $fillable = [
+        'soh_id',
+        'barang_id',
+        'catatan',
+        'created_by',
+        'tgl_opname',
+        'principal',
+        'updated_at'
+    ];
+
+    public function barang()
+    {
+        return $this->belongsTo(BarangWfgModel::class, 'barang_id');
+    }
+
+    public function soh()
+    {
+        return $this->belongsTo(StockOnHandModel::class, 'soh_id', 'id');
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
---
+++ database/migrations/2025_10_30_091628_create_wfg_sop_temp_note_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wfg_sop_temp_note', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('soh_id')->nullable()->constrained('wfg_soh')->onDelete('cascade');
+            $table->foreignId('barang_id')->constrained('wfg_barang')->onDelete('cascade');
+            $table->string('catatan')->nullable();
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
+            $table->date('tgl_opname')->nullable();
+            $table->string('principal')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wfg_sop_temp_note');
+    }
+};
