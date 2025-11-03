04c65f8dd277d247d295817574b934ff76d9934d
Tohadi <toadiasley243@gmail.com>
2025-11-03 13:36:02 +0700

    add wfg sop status

---
+++ database/migrations/2025_10_31_134219_create_wfg_sop_status_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wfg_sop_status', function (Blueprint $table) {
+            $table->id();
+            $table->date('tgl_opname');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('status')->default('draft');
+            $table->string('principal')->nullable();
+            $table->timestamps();
+
+            $table->unique(['user_id', 'tgl_opname']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wfg_sop_status');
+    }
+};
