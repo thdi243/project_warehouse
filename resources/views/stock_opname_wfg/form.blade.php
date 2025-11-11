e8bedfc265677cc2d9b1e3ced191aa47f864de3e
Tohadi <toadiasley243@gmail.com>
2025-11-11 16:27:58 +0700

    fix: simplify AJAX request URL for stock opname status

--- resources/views/stock_opname_wfg/form.blade.php
+++ resources/views/stock_opname_wfg/form.blade.php
@@ -383,7 +383,7 @@
     <script>
         $(document).ready(function() {
 
-            $.get(url('/wfg/stock_opname/sop/status'), function(res) {
+            $.get('/wfg/stock_opname/sop/status', function(res) {
                 const btn = $('#btnStartOpname');
 
                 if (res.status === 'started') {
