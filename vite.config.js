e98f0df86bd9edd2bf4f11fdfc473ec8e356860d
Tohadi <toadiasley243@gmail.com>
2025-12-16 15:18:56 +0700

    feat: update dependencies and enhance Tailwind CSS configuration
    - Updated package.json to include new dependencies from Radix UI and other libraries.
    - Enhanced app.css with custom CSS variables for theming and dark mode support.
    - Modified tailwind.config.js to support dark mode and added custom color and border radius configurations.
    - Updated vite.config.js to use React plugin and set up path aliasing for cleaner imports.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import path from 'path'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/js/app.jsx'
+            ],
             refresh: true,
         }),
+        react({
+            fastRefresh: false 
+        }),
     ],
+    resolve: {
+        alias: {
+        '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
 });
