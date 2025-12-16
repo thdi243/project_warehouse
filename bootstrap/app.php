b2b902bebe3d9628405849bfc0874edb69acf93c
Tohadi <toadiasley243@gmail.com>
2025-12-16 15:19:56 +0700

    refactor: remove unused CORS configuration and tidy up app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'access' => AccessByJabatan::class,
         ]);
     })
+
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
