55ecdd38492cd62a517c1828b4efd6b87167e4d7
Tohadi <toadiasley243@gmail.com>
2025-12-17 09:53:23 +0700

    add build

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,28 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/js/app.jsx'
-            ],
-            refresh: true,
-        }),
-        react({ fastRefresh: false }),
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'resources/js'),
-        },
+  plugins: [
+    laravel({
+      input: ['resources/js/app.jsx'],
+      refresh: true,
+    }),
+    react({ fastRefresh: false }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'resources/js'),
     },
-    build: {
-        manifest: true,          
-        outDir: 'public/build',  
-        emptyOutDir: true,
+  },
+  build: {
+    manifest: true,
+    outDir: 'public/build',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
     },
+  },
 });
