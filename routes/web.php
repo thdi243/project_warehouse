2cdfa7f06d21dbf1f7105f461bafe4bc23ded48c
Tohadi <toadiasley243@gmail.com>
2025-12-18 09:49:40 +0700

    testing

--- routes/web.php
+++ routes/web.php
@@ -287,7 +287,7 @@ Route::middleware('auth')->group(function () {
         Route::delete('/notifications/delete-all', [NotificationController::class, 'destroyAll'])->name('notifications.delete-all');
     });
 
-    Route::get('/app/{any?}', function () {
+    Route::get('/app/test', function () {
         // return view('app');
         return 'LARAVEL OK';
     })->where('any', '.*');
