a9a43d83c27c43953cf34465157fca46df3684ef
Tohadi <toadiasley243@gmail.com>
2026-06-30 11:37:01 +0700

    fix: Update route checks in master WPM sidebar for consistency

--- resources/views/layouts/partials/sidebar/master_wpm.blade.php
+++ resources/views/layouts/partials/sidebar/master_wpm.blade.php
@@ -1,18 +1,18 @@
 @can('permission', 'master-wpm')
     <li class="nav-item">
-        <a class="nav-link menu-link {{ request()->routeIs('wpm.master.*') ? 'collapsed' : '' }}" href="#sidebarMasterWpm"
-            data-bs-toggle="collapse" role="button"
-            aria-expanded="{{ request()->routeIs('wpm.master.*') ? 'true' : 'false' }}" aria-controls="sidebarMasterWpm">
+        <a class="nav-link menu-link {{ request()->Is('master/wpm.*') ? 'collapsed' : '' }}" href="#sidebarMasterWpm"
+            data-bs-toggle="collapse" role="button" aria-expanded="{{ request()->Is('master/wpm.*') ? 'true' : 'false' }}"
+            aria-controls="sidebarMasterWpm">
             <i class="bx bx-package"></i>
             <span data-key="t-stock_op_wpm">Master WPM</span>
         </a>
 
-        <div class="collapse menu-dropdown {{ request()->routeIs('wpm.master.*') ? 'show' : '' }}" id="sidebarMasterWpm">
+        <div class="collapse menu-dropdown {{ request()->Is('master/wpm.*') ? 'show' : '' }}" id="sidebarMasterWpm">
             <ul class="nav nav-sm flex-column">
                 @can('permission', 'wpm-master-barang')
                     <li class="nav-item">
-                        <a href="{{ route('wpm.master.barang.index') }}"
-                            class="nav-link {{ request()->routeIs('wpm.master.barang.index') ? 'active' : '' }}"
+                        <a href="{{ route('master.wpm.barang.index') }}"
+                            class="nav-link {{ request()->Is('master.wpm.barang.index') ? 'active' : '' }}"
                             data-key="t-input-tkbm">
                             <i class="bx bx-git-commit fs-12"></i>Master Barang
                         </a>
