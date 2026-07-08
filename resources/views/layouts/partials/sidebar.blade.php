b9ff027116195fd1bd5843e0b2891574ea8429c4
Tohadi <toadiasley243@gmail.com>
2026-07-08 13:59:36 +0700

    feat: change if condition sidebar no warehouse

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -38,7 +38,7 @@
         <div class="container-fluid">
             <div id="two-column-menu">
             </div>
-            @if (!in_array($departemen, ['produksi', 'quality_control', 'engineering', 'warehouse', 'it', 'IT']))
+            @if ($departemen != 'warehouse')
                 <ul class="navbar-nav" id="navbar-nav">
                     @include('layouts.partials.sidebar-no-warehouse.sidebar_no_warehouse')
 
