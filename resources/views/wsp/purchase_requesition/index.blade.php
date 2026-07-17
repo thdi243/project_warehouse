5d4f9be3a927171ffb7c6ea8b75534fb4cbd901b
Tohadi <toadiasley243@gmail.com>
2026-07-17 12:47:43 +0700

    fix: remove unused 'FLAG' column and clean up item detail rendering

--- resources/views/wsp/purchase_requesition/index.blade.php
+++ resources/views/wsp/purchase_requesition/index.blade.php
@@ -198,7 +198,7 @@
                         </div>
                     </div>
                     <div class="table-responsive">
-                        <table class="table table-hover table-borderedless align-middle" id="IncomingTable">
+                        <table class="table table-hover table-borderedless align-middle">
                             <thead class="table-light text-nowrap">
                                 <tr>
                                     <th class="text-center" style="width: 60px;">NO</th>
@@ -208,7 +208,7 @@
                                     <th>NAMA PEMINTA</th>
                                     <th>DEPARTEMEN</th>
                                     <th>STATUS APPROVED</th>
-                                    <th>FLAG</th>
+                                    {{-- <th>FLAG</th> --}}
                                     <th class="text-center text-nowrap">AKSI</th>
                                     @can('permission', 'wsp-data-pr-plus')
                                         <th class="text-center">AKSI WSP</th>
@@ -608,7 +608,6 @@
                                     ${statusText}
                                 </span>
                             </td>
-                            <td>${flagText}</td>
                             <td class="text-nowrap">
                                 <div class="d-flex justify-content-center gap-1">
 
@@ -861,16 +860,16 @@
                                 <td>${item.barang?.uom ?? '-'}</td>
                                 <td>
                                     ${item.keterangan ? `
-                                                                    <div class="d-flex align-items-center justify-content-between gap-2">
-                                                                        <span>${item.keterangan}</span>
-                                                                        <button class="btn btn-sm btn-link p-0 text-secondary border-0 btn-copy-keterangan" 
-                                                                                style="flex-shrink: 0;"
-                                                                                data-text="${escapeHtmlAttribute(item.keterangan)}"
-                                                                                title="Copy Keterangan">
-                                                                            <i class="mdi mdi-content-copy"></i>
-                                                                        </button>
-                                                                    </div>
-                                                                ` : '-'}
+                                                                        <div class="d-flex align-items-center justify-content-between gap-2">
+                                                                            <span>${item.keterangan}</span>
+                                                                            <button class="btn btn-sm btn-link p-0 text-secondary border-0 btn-copy-keterangan" 
+                                                                                    style="flex-shrink: 0;"
+                                                                                    data-text="${escapeHtmlAttribute(item.keterangan)}"
+                                                                                    title="Copy Keterangan">
+                                                                                <i class="mdi mdi-content-copy"></i>
+                                                                            </button>
+                                                                        </div>
+                                                                    ` : '-'}
                                 </td>
                                 <td><span class="badge ${badgeClass}">${jenisText}</span></td>
                                 <td>${item.alasan ?? '-'}</td>
@@ -1018,10 +1017,10 @@
                                 </div>
 
                                 ${a.catatan ? `
-                                                    <div class="small mt-1">
-                                                        Catatan: ${a.catatan}
-                                                    </div>
-                                                ` : ''}
+                                                        <div class="small mt-1">
+                                                            Catatan: ${a.catatan}
+                                                        </div>
+                                                    ` : ''}
                             </div>
 
                         </div>
