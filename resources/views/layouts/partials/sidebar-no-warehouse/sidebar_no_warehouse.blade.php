129eb3bc353b04e3bb7cfa0702d2c623580552c1
Tohadi <toadiasley243@gmail.com>
2026-07-20 08:38:03 +0700

    feat: Allow supervisor role to access PR approval section in sidebar

--- resources/views/layouts/partials/sidebar-no-warehouse/sidebar_no_warehouse.blade.php
+++ resources/views/layouts/partials/sidebar-no-warehouse/sidebar_no_warehouse.blade.php
@@ -10,7 +10,7 @@
             class="nav-link menu-link {{ request()->routeIs('stock.pr.history') ? 'active' : '' }}">
             <i class="bx bx-git-commit fs-12"></i>Data Riwayat PR</a>
     </li>
-    @if ($jabatan == 'dept_head')
+    @if ($jabatan == 'dept_head' || $jabatan == 'supervisor')
         <li class="nav-item">
             <a href="{{ route('stock.pr.approval') }}"
                 class="nav-link menu-link {{ request()->routeIs('stock.pr.approval') ? 'active' : '' }}">
