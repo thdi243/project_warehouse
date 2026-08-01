dc90b831083e61c92b866a3162ed75762d32c731
Tohadi <toadiasley243@gmail.com>
2026-08-01 10:50:58 +0700

    feat: Add export route for Stock On Hand data in StockOnHandController

--- routes/web.php
+++ routes/web.php
@@ -91,6 +91,7 @@ Route::middleware('auth')->group(function () {
         // Stock On Hand
         Route::get('/stock-on-hand', [WarehouseController::class, 'stockOnHandView'])->name('stock.soh.user');
         Route::get('/get-data', [StockOnHandController::class, 'getDataSOH'])->name('stock.soh_data');
+        Route::get('/export', [StockOnHandController::class, 'exportExcel'])->name('stock.soh_export');
     });
     // End Free access
 
@@ -127,6 +128,8 @@ Route::middleware('auth')->group(function () {
             Route::get('/vehicle', [VehicleTrackingController::class, 'dashboard'])->name('dashboard.vehicle')
                 ->middleware(['permission:dashboard-vehicle-monitoring']);
             Route::get('/vehicle/data', [VehicleTrackingController::class, 'dashboardData'])->name('dashboard.vehicle.data')
+                ->middleware(['permission:dashboard-vehicle-monitoring']);
+            Route::get('/vehicle/visual', [VehicleTrackingController::class, 'visualDashboard'])->name('dashboard.vehicle.visual')
                 ->middleware(['permission:dashboard-vehicle-monitoring']);
 
             // Stock Opname Dashboard
@@ -194,7 +197,6 @@ Route::middleware('auth')->group(function () {
                 Route::post('/store', [StockOnHandController::class, 'store'])->name('stock.soh_store');
                 Route::put('/update/{id}', [StockOnHandController::class, 'update'])->name('stock.soh_update');
                 Route::delete('/delete/{id}', [StockOnHandController::class, 'destroy'])->name('stock.soh_delete');
-                Route::get('/export', [StockOnHandController::class, 'exportExcel'])->name('stock.soh_export');
                 Route::get('/download', [StockOnHandController::class, 'downloadTemplate'])->name('stock.soh_download');
                 Route::post('/upload', [StockOnHandController::class, 'upload'])->name('stock.soh_upload');
                 Route::get('/opname', [WarehouseController::class, 'opnameIndex'])->name('stock.opname');
