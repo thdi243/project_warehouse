1a53cf5956d4e8f14800b50f663fca6e5102d3b0
Tohadi <toadiasley243@gmail.com>
2026-09-14 10:50:21 +0700

    fix: update WebSocket configuration to use HTTPS protocol and disable stats

--- public/js/app.js
+++ public/js/app.js
@@ -479,6 +479,8 @@ $(document).ready(function () {
         if (typeof window.Echo === "function" && window.AppConfig) {
             window.Pusher = Pusher;
 
+            const isHttps = window.location.protocol === "https:" || Boolean(window.AppConfig.reverb && window.AppConfig.reverb.forceTLS);
+
             window.Echo = new window.Echo({
                 broadcaster: "reverb",
                 key: window.AppConfig.reverb.key,
@@ -486,8 +488,9 @@ $(document).ready(function () {
                     window.AppConfig.reverb.wsHost || window.location.hostname,
                 wsPort: window.AppConfig.reverb.wsPort,
                 wssPort: window.AppConfig.reverb.wssPort,
-                forceTLS: window.AppConfig.reverb.forceTLS,
+                forceTLS: isHttps,
                 enabledTransports: ["ws", "wss"],
+                disableStats: true,
             });
         }
 
